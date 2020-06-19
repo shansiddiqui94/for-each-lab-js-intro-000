aca9ede08631e10543e61c4afab1eef6a64ef5f2
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-06-19 18:33:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ function iterativeLog(array){
 }
 
 
-function iterate(function(array)){
-  array.forEach(array){
+function iterate(array){
+
+  array.forEach(function(array)){
 
   }
+  return array
 }
 
 // var evens = [0, 2, 4, 6, 8, 10];
