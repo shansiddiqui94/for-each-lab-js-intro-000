09077217e546bfcd9698d896eecdf352710e409f
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-06-19 18:25:27 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function iterativeLog(array){
+
+  array.forEach(function(element, index){
+                // callback function
+                //automatically ran inside any function
+    console.log(`${index}: ${element}`)
+
+  })
+}
+
+function iterate(function(array)){
+  array.forEach(array){
+
+  }
+}
+
+// var evens = [0, 2, 4, 6, 8, 10];
+//
+// evens.forEach((even, index, array) => {
+//   debugger;
+//   console.log(`${even} is not odd!`);
+// });
