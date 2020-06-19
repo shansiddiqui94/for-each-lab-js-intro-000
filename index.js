d81db2c2ea515775f61c185bfd1ca20955a63182
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-06-19 18:43:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,33 @@
+function iterativeLog(array){
+
+  array.forEach(function(element, index){
+                // callback function
+                //automatically ran inside any function
+    console.log(`${index}: ${element}`)
+
+  })
+}
+
+
+function iterate(callback){
+
+const array = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+array.forEach(callback)
+
+return array
+}
+
+
+function doToArray(array, callback){
+  array.forEach(callback)
+}
+
+
+
+// var evens = [0, 2, 4, 6, 8, 10];
+//
+// evens.forEach((even, index, array) => {
+//   debugger;
+//   console.log(`${even} is not odd!`);
+// });
